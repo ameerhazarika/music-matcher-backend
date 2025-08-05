daa0dc90148030ad29fbf32dd8ff2319fb752b8e
ameerhazarika <ameer.hazarika@gmail.com>
2025-08-05 07:02:58 +0530

    change localhost url to react frontend production url

--- src/main/java/com/musicmatch/config/SecurityConfig.java
+++ src/main/java/com/musicmatch/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // React frontend
+                        .allowedOrigins("https://music-matcher-frontend.vercel.app") // React frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
