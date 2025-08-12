e5f6745a308192d2cbb2a330babbcced0193454b
ameerhazarika <ameer.hazarika@gmail.com>
2025-08-13 05:21:58 +0530

    combined allowed origins localhost and vercel

--- src/main/java/com/musicmatch/config/SecurityConfig.java
+++ src/main/java/com/musicmatch/config/SecurityConfig.java
@@ -52,8 +52,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // React frontend local
-                        .allowedOrigins("https://music-matcher-frontend.vercel.app") // React frontend
+                        .allowedOrigins("https://music-matcher-frontend.vercel.app","http://localhost:5173") // React frontend local
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
